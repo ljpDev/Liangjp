384d9433a6280060b9d85d8dcc78cc6e3838dc58
Liangjp <ljpdev@gmail.com>
2013-03-26 19:03:26 +0800

    add new files

---
+++ hzcLibShow/hzcLibShow/HZCLCustomNavController.h
@@ -0,0 +1,13 @@
+//
+//  HZCLCustomNavController.h
+//  hzcLibShow
+//
+//  Created by GuGu on 13-3-25.
+//  Copyright (c) 2013年 Jiaping Liang. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface HZCLCustomNavController : UINavigationController
+
+@end
---
+++ hzcLibShow/hzcLibShow/HZCLCustomNavController.m
@@ -0,0 +1,38 @@
+//
+//  HZCLCustomNavController.m
+//  hzcLibShow
+//
+//  Created by GuGu on 13-3-25.
+//  Copyright (c) 2013年 Jiaping Liang. All rights reserved.
+//
+
+#import "HZCLCustomNavController.h"
+
+@interface HZCLCustomNavController ()
+
+@end
+
+@implementation HZCLCustomNavController
+
+- (id)initWithNibName:(NSString *)nibNameOrNil bundle:(NSBundle *)nibBundleOrNil
+{
+    self = [super initWithNibName:nibNameOrNil bundle:nibBundleOrNil];
+    if (self) {
+        // Custom initialization
+    }
+    return self;
+}
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+	// Do any additional setup after loading the view.
+}
+
+- (void)didReceiveMemoryWarning
+{
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
