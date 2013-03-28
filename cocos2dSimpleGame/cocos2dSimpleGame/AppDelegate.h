a09661114f54c26659900485bc6ada5278168674
Liangjp <ljpdev@gmail.com>
2013-03-28 23:29:12 +0800

    init new cocos2d program

---
+++ cocos2dSimpleGame/cocos2dSimpleGame/AppDelegate.h
@@ -0,0 +1,28 @@
+//
+//  AppDelegate.h
+//  cocos2dSimpleGame
+//
+//  Created by GuGu on 13-3-26.
+//  Copyright Liangjp 2013年. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "cocos2d.h"
+
+// Added only for iOS 6 support
+@interface MyNavigationController : UINavigationController <CCDirectorDelegate>
+@end
+
+@interface AppController : NSObject <UIApplicationDelegate>
+{
+	UIWindow *window_;
+	MyNavigationController *navController_;
+
+	CCDirectorIOS	*director_;							// weak ref
+}
+
+@property (nonatomic, retain) UIWindow *window;
+@property (readonly) MyNavigationController *navController;
+@property (readonly) CCDirectorIOS *director;
+
+@end
